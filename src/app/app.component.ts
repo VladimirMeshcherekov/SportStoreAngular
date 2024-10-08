eabe5b35c924aab2277aea096ef47d7a04799163
Snapp <3gotosnapp@gmail.com>
2024-10-08 21:57:38 +0300

    init 7.11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app",
+  template: `
+    <div class="bg-success p-a-1 text-xs-center">
+      This is SportsStore
+    </div>`
 })
 export class AppComponent {
-  title = 'SportStore';
 }
